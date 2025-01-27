1da66867d56eab323b183e06c4668ee78c00da7a
sushantkhilare25 <118913289+sushantkhilare25@users.noreply.github.com>
2025-01-27 13:03:42 +0530

    Add 1 more Printing Statement

--- Student.java
+++ Student.java
@@ -2,8 +2,9 @@ public class Student
 {
 
    public static void main(String[] args)
-  {
-     
+   {
     System.out.println("Sushant learning Git Bash..!");
+
+      System.out.println("Git Bash run Successfull ...!");
 }
 }
